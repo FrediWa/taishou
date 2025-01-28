17f72eb5f5c9a255822a7bd77ab8fc3e2a8fc623
Fredrik Wasström <fredrik.wasstrom@gmail.com>
2025-01-28 14:01:31 +0200

    init

---
+++ test/keymap.c
@@ -0,0 +1,13 @@
+#include QMK_KEYBOARD_H
+
+#define KCX_CMD MO(_CMD)
+
+enum layer_number {
+  _QWERTY = 0,
+  _CMD,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_QWERTY] = LAYOUT(KC_A, KCX_CMD),
+    [_CMD] = LAYOUT(KC_RIGHT)
+};
